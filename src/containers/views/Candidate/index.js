eccdf44f1c6e7185911f3a120899bb70263e5862
Steven Blake <s.blake.191289@gmail.com>
2021-05-23 20:35:12 +0100

    Added back button

--- src/containers/views/Candidate/index.js
+++ src/containers/views/Candidate/index.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { Card, Container, Tab, Tabs } from "react-bootstrap";
+import { Card, Container, Tab, Tabs, Button } from "react-bootstrap";
 import SkillsTab from "./tabs/Skills";
 import DetailsTab from "./tabs/Details";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 
 
 const CandidateScreen = () => {
   const { id } = useParams();
+  const history = useHistory();
 
   return (
     <Container className="main-container">
 
-      <Card>
+      <Button onClick={() => history.push('/candidates')}>Back to candidates</Button>
+
+      <Card className="mt-1">
         <Card.Body>
           <Tabs fill variant="pills" defaultActiveKey="details" style={{ marginBottom: 5 }}>
             <Tab eventKey="details" title="Details">
