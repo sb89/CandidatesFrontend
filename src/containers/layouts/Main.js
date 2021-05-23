7d3ed108ec463a2a307513a9202f248d3e8af8cd
Steven Blake <s.blake.191289@gmail.com>
2021-05-23 10:29:09 +0100

    Added home screen

--- src/containers/layouts/Main.js
+++ src/containers/layouts/Main.js
@@ -1,8 +1,23 @@
+import React from "react";
+import { Container, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+
 const Main = ({ children }) => {
   return (
-    <div>
-      {children}
-    </div>
+    <React.Fragment>
+      <Navbar bg="primary" expand="lg" id="main-nav">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Candidates Application</Navbar.Brand>
+          </LinkContainer>
+
+        </Container>
+      </Navbar>
+      <div>
+        {children}
+      </div>
+    </React.Fragment>
   );
 };
 
--- src/containers/views/Home/index.js
+++ src/containers/views/Home/index.js
@@ -1,6 +1,13 @@
+import { Container, Jumbotron } from "react-bootstrap";
+
 const Home = () => {
   return(
-    <div>Home</div>
+    <Container fluid className="main-container">
+      <Jumbotron className="text-center">
+        <h1>Welcome to the Candidates Application</h1>
+      </Jumbotron>
+    </Container>
+
   );
 };
 
